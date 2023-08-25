f60d59ef042b1d3e17ad4a57577792cd0aeab3d2
GUILHERME VIEIRA FIRMEZA <202301135737@alunos.ibmec.edu.br>
2023-08-25 11:26:52 -0300

    adicionando AC3

---
+++ ACs/AC3/src/Aluno.java
@@ -0,0 +1,8 @@
+public class Aluno {
+    
+    String nome;
+
+    Aluno(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ ACs/AC3/src/Palestrante.java
@@ -0,0 +1,9 @@
+public class Palestrante {
+    String nome;
+    String palestra;
+
+    Palestrante(String nome, String palestra) {
+        this.nome = nome;
+        this.palestra = palestra;
+    }
+}
